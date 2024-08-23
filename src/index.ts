aa1a6861c2de1687e09e4ec04cee920c29890a48
b-avb <brayan.vargas.dev@gmail.com>
2024-08-22 22:37:20 -0500

    feat: withdraw

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { XcmProgramsExecutor } from "./executor";
 import { topupThenCreateCommunity } from "./top-up-then-create-community";
+import { topupThenWithdraw } from "./top-up-then-withdraw";
 import { ExecutableFunctionOf } from "./types";
 
 export { XcmProgramsExecutor } from "./executor";
 
 export function globallyInjectPrograms(executor: XcmProgramsExecutor) {
-  const functions: Function[] = [topupThenCreateCommunity];
+  const functions: Function[] = [
+    topupThenCreateCommunity,
+    topupThenWithdraw
+  ];
 
   for (const fn of functions) {
     globalThis[fn.name] = (...params: any[]) =>
